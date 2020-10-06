cb98c29339e9f86cddb91486ddbb18d45edbbf09
SuperBrainBro <superbrainstormers@gmail.com>
2020-10-05 18:06:36 -0700

    new project

---
+++ 2Timothy1Seven/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2Timothy1Seven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
