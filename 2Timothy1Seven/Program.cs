4479e39a751c08508c56f33f74e31d4a7d1a5a76
SuperBrainBro <superbrainstormers@gmail.com>
2020-10-05 20:20:55 -0700

    beta 1

--- 2Timothy1Seven/Program.cs
+++ 2Timothy1Seven/Program.cs
@@ -13,37 +13,47 @@ namespace _2Timothy1Seven
             public static string name;
         }
         public static void Main()
-        {
+        {           
             Startup();
         }
         public static void Startup()
         {
-            if (Config.name == null)
+            if (Config.name == "100")
             {
                 Configuration();
             } else
             {
-                Console.WriteLine("");
-                Console.WriteLine("");
                 Console.WriteLine("Welcome to 2Timothy1Seven!" + Config.name);
+                Console.Beep();
+                Console.WriteLine("By Vincent Jenei (Lite #4211)");
+                Console.Beep();
                 Console.WriteLine("");
+                Console.Beep();
                 Console.WriteLine("For God has not given us a spirit of fear, but of power and of love and of a sound mind.");
+                Console.Beep();
                 Console.WriteLine("");
+                Console.Beep();
                 Console.WriteLine("Today: " + thisDate);
+                Console.Beep();
                 Console.WriteLine("");
+                Console.Beep();
                 Console.WriteLine("");
+                Console.Beep();
             }
             Console.Write("Press enter key to exit.");
+            Console.Beep();
             Console.ReadLine();
         }
 
         public static void Configuration()
         {
             Console.Write("Enter your name: ");
-            name = Console.ReadLine();
-            Console.Write("Good day, ");
-            Console.Write(name);
-            Console.WriteLine("!");
+            Console.Beep();
+            Config.name = Console.ReadLine();;
+            Console.WriteLine("Configuration Complete.");
+            Console.Beep();
+            Console.WriteLine("");
+            Console.Beep();
         }
     }
 }
